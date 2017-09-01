6b837706a2315551ed6772796ca80ff3b9107d10
duxiaofeng <297951292@qq.com>
2017-09-01 13:23:29 +0800

    fix log

--- backup-blog.sh
+++ backup-blog.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+nowWithSecond=$(date +%Y\-%m\-%d\ %H\:%M\:%S)
+
+echo $nowWithSecond
+
 docker commit $(docker ps | grep blog | awk '{print $1}') 297951292/blog
-docker push 297951292/blog
-docker rmi -f $(docker images | grep none | awk '{print $3}')
+docker push 297951292/blog > /dev/null
+docker system prune -f
+
+echo 'done'
