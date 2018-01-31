785b581c79a12b1307ed75418aa8bb09b7910422
duxiaofeng <csdxf297951292@gmail.com>
2018-01-31 16:24:58 +0800

    fix backup script

--- backup-blog.sh
+++ backup-blog.sh
@@ -7,6 +7,10 @@ echo $nowWithSecond
 
 docker commit $(docker ps | grep blog | awk '{print $1}') 297951292/blog:$tag
 docker push 297951292/blog:$tag > /dev/null
+
+docker tag 297951292/blog:$tag 297951292/blog:latest
+docker push 297951292/blog:latest > /dev/null
+
 docker system prune -f
 
 echo 'done'
