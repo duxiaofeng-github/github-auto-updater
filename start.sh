dd3c9e18756b4a1663f5b5dd7460ff5df122f994
duxiaofeng <csdxf297951292@gmail.com>
2018-01-31 15:39:21 +0800

    fix script

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@ docker login
 docker run -d --name blog -p 80:80 -p 8388:8388 297951292/blog nginx
 
 hasCrontab=$(crontab -l | grep "/home/johnny/github-auto-updater/backup-blog.sh")
+cron=$'0 0 * * * /home/johnny/github-auto-updater/backup-blog.sh >> /home/johnny/backup-blog.log\n0 0 * * * /home/johnny/github-auto-updater/github-updater.sh >> /home/johnny/github-updater.log'
 
-[ "$hasCrontab"=="" ] && echo "0 0 * * * /home/johnny/github-auto-updater/backup-blog.sh >> /home/johnny/backup-blog.log" | crontab - && echo "0 0 * * * /home/johnny/github-auto-updater/github-updater.sh >> /home/johnny/github-updater.log" | crontab -
+[ "$hasCrontab"=="" ] && echo "$cron" | crontab -
