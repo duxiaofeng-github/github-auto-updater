d78747816501d81adbd7f59863258873782ae036
duxiaofeng <csdxf297951292@gmail.com>
2018-01-31 16:19:56 +0800

    fix backup script

--- backup-blog.sh
+++ backup-blog.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 nowWithSecond=$(date +%Y\-%m\-%d\ %H\:%M\:%S)
+tag=$(date -I)
 
 echo $nowWithSecond
 
-docker commit $(docker ps | grep blog | awk '{print $1}') 297951292/blog
-docker push 297951292/blog > /dev/null
+docker commit $(docker ps | grep blog | awk '{print $1}') 297951292/blog:$tag
+docker push 297951292/blog:$tag > /dev/null
 docker system prune -f
 
 echo 'done'
