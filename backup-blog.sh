245717e5be4c8e92ad6dc00fa25229c3849ad765
duxiaofeng <csdxf297951292@gmail.com>
2018-03-21 12:03:33 +0800

    update script

--- backup-blog.sh
+++ backup-blog.sh
@@ -11,11 +11,9 @@ docker push 297951292/blog:$tag > /dev/null
 docker tag 297951292/blog:$tag 297951292/blog:latest
 docker push 297951292/blog:latest > /dev/null
 
-images=docker images | awk 'NR>3{print $3}' | sed '$d' | sed '$d'
+docker system prune -f
 
-if [ $images ]; then
-    docker rmi images
-fi
-
+images=$(docker images | awk 'NR>3{print $3}' | sed '$d' | sed '$d')
+if [ $images ]; then docker rmi $images; fi
 
 echo 'done'
