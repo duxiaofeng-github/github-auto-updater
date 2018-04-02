60918477224faf844d10e883a6d03fadc8ed3a7c
duxiaofeng <csdxf297951292@gmail.com>
2018-04-02 11:31:12 +0800

    update script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 docker login
-docker run -d --name blog -p 80:80 -p 8388:8388 297951292/blog nginx
+docker run -d --name blog -p 80:80 -p 13425:13425 297951292/blog nginx
 
 hasCrontab=$(crontab -l | grep "/home/johnny/github-auto-updater/backup-blog.sh")
 cron=$'0 0 * * * /home/johnny/github-auto-updater/backup-blog.sh >> /home/johnny/backup-blog.log\n0 0 * * * /home/johnny/github-auto-updater/github-updater.sh >> /home/johnny/github-updater.log'
