805db6a13b6507c3f403efb1868c0c7689e5f85b
duxiaofeng-github <297951292@qq.com>
2019-08-06 23:04:42 +0800

    fix bug

--- get-player-script-url.py
+++
@@ -1,9 +0,0 @@
-import urllib2
-import re
-
-contents = urllib2.urlopen("https://cloud.tencent.com/document/product/267/7479").read()
-
-m = re.search('<span class="hljs-string">"(.*?)"</span>', contents)
-
-if m :
-	print('https:' + m.group(1))
